07823d422b1872fe8751e2828a2308caf171f64e
Shamit187 <shamit187@gmail.com>
2023-09-22 01:18:39 +0600

    Clock

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,11 +1,43 @@
-pub struct Clock;
+use std::fmt;
+#[derive(Debug, PartialEq)]
+pub struct Clock{
+    minutes: i32,
+}
 
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        todo!("Construct a new Clock from {hours} hours and {minutes} minutes");
+        Clock {
+            minutes: (hours * 60 + minutes).rem_euclid(24 * 60),
+        }
+
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
-        todo!("Add {minutes} minutes to existing Clock time");
+        Clock {
+            minutes: (self.minutes + minutes).rem_euclid(24 * 60),
+        }
     }
 }
+
+impl fmt::Display for Clock {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let hours = self.minutes / 60;
+        let minutes = self.minutes % 60;
+        write!(f, "{:02}:{:02}", hours, minutes)
+    }
+}
+
+// impl fmt::Debug for Clock {
+//     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+//         f.debug_struct("Clock")
+//             .field("minutes", &self.minutes)
+//             .finish()
+//     }
+// }
+
+// impl PartialEq for Clock {
+//     fn eq(&self, other: &Self) -> bool {
+//         self.minutes == other.minutes
+//     }
+// }
+
