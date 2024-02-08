7244ba1e7a714e577a4d0430b549f03a8d243108
Shamit187 <shamit187@gmail.com>
2024-02-08 23:41:31 +0600

    Ok

--- trying_out/src/main.rs
+++ trying_out/src/main.rs
@@ -1,17 +1,16 @@
 use std::thread;
-use std::time::Duration;
+// use std::time::Duration;
 
 fn main() {
-    let handle = thread::spawn(||{
-        for i in 1..10 {
-            println!("hi number {} from spawned thread", i);
-            thread::sleep(Duration::from_millis(1));
-        }
+    let v = vec![1,2,3];
+
+    let handle = thread::spawn(move||{
+        println!("Hello");
+        println!("Let me use v");
+        println!("{:?}: Hey Vector",v);
     });
 
+    // println!("{:?}: I will still use the vector", v);
+
     handle.join().unwrap();
-    for i in 1..5 {
-        println!("hi number {} from main thread", i);
-        thread::sleep(Duration::from_millis(1));
-    }
 }
